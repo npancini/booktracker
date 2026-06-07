7da9f0496dae5a772dcb8f98d74c259dd7fd35da
Nicholas Pancini <nicholaspancini@Nicholass-Air-3.lan>
2026-06-07 19:52:36 -0400

    Fix DATABASE_URL handling for local + production

--- config/settings.py
+++ config/settings.py
@@ -91,9 +91,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
